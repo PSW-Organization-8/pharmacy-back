9ecc8d71f03849c1ffac82c71a621f550fa1e120
MatijaUskokovic <uskokovic.ra76.2018@uns.ac.rs>
2021-12-07 14:57:56 +0100

    feat: implemented compression of old pdf files
    feat: implemented background service
    
    feat: implemented pdf compression without ckecks

--- PharmacyInformationSystem/PharmacyAPI/Startup.cs
+++ PharmacyInformationSystem/PharmacyAPI/Startup.cs
@@ -7,6 +7,7 @@ using PharmacyClassLib.Repository;
 using PharmacyClassLib.Service;
 using PharmacyClassLib;
 using Microsoft.EntityFrameworkCore;
+using PharmacyAPI.BackgroundService;
 using PharmacyClassLib.Model;
 using PharmacyClassLib.Repository.MedicationIngredientRepository;
 using PharmacyClassLib.Repository.MedicationIngredientsRepository;
@@ -73,6 +74,7 @@ namespace WebApplication1
 
             services.AddScoped<MedicationConsumptionService>();
 
+            services.AddHostedService<CompressionOfOldFiles>();
         }
 
         private Server server;
